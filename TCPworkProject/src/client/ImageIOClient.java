1c09e7bff77d835edab2b279af1f1ea9cc50997d
Yamaguchi <Yamaguchi@192.168.101.104>
2013-08-07 13:57:59 +0900

    byte配列通信途中まで
    少しやり方をかえてみる

--- TCPworkProject/src/client/ImageIOClient.java
+++ TCPworkProject/src/client/ImageIOClient.java
@@ -1,73 +1,71 @@
 package client;
 
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBuffer;
+import java.awt.image.WritableRaster;
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 import javax.imageio.ImageIO;
 
 public class ImageIOClient {
 	public static void main(String[] args) throws Exception{
-
 		new ImageIOClient().exec(args);
 	}
 
 	public void exec(String[] args) throws Exception{
 
-		String srcFilePath = "D:/tmp/001.bmp";
-		File srcFile = new File(srcFilePath);
-		BufferedImage srcImg = ImageIO.read(srcFile);
-		
-		if((args.length < 2)){
+		if((args.length < 1)){
 			throw new IllegalArgumentException("Parameter(s) : <Server> [<Port>]");
 		}
 
+		//ソケット接続
 		String server = args[0];
 		int servPort = (args.length == 2)? Integer.parseInt(args[1]) :10514;
-		Socket sock = null;
-
-		try {
-			sock = new Socket(server, servPort);
-		} catch (UnknownHostException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
+		Socket sock = new Socket(server, servPort);
 		System.out.println("Connected to server ...sending echo string");
 
-		InputStream in = null;
-		try {
-			in = sock.getInputStream();
-		} catch (IOException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
+		InputStream in = sock.getInputStream();
+		OutputStream out = sock.getOutputStream();
 
-		OutputStream out = null;
-		try {
-			out = sock.getOutputStream();
-		} catch (IOException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
-		}
-		
-		
-		ImageIO.write(srcImg, "bmp", out);
-		
-		
-		
-		
-		try {
+		String srcDirPath = "C:/Users/Yamaguchi/git/TCPwork/TCPworkProject/resources/debug/src";
+		File srcDir = new File(srcDirPath);
+		File srcFiles[] = srcDir.listFiles();
+		for(File srcFile : srcFiles){
+			
+			BufferedImage srcImg = ImageIO.read(srcFile);
+			WritableRaster srcRas = srcImg.getRaster();
+			DataBuffer srcBuf = srcRas.getDataBuffer();
+			int w = srcImg.getWidth();
+			int h = srcImg.getHeight();
+			
+			byte srcBytes[] = new byte[w*h];
+			for(int i = 0; i < h*w; i++){
+				srcBytes[i] = (byte) srcBuf.getElem(i);
+			}
+			ImageIO.write(srcImg, "bmp", out);
+			
+			int totalBytesRcvd = 0;
+			int bytesRcvd;
+			
+			while(totalBytesRcvd < srcBuf.getSize()){
+				if((bytesRcvd = in.read(srcBytes,totalBytesRcvd,srcBuf.getSize() - totalBytesRcvd)) == -1);
+			}
+			
+
+
+
+
+
+
+
+
+
+
+
 			sock.close();
-		} catch (IOException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
 		}
 	}
 }
