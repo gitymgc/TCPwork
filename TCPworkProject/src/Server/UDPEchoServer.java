55e1e405267a88d62b311047557468e37b31e347
Yamaguchi <Yamaguchi@192.168.101.104>
2013-08-07 10:41:26 +0900

    ひとまず完成

--- TCPworkProject/src/Server/UDPEchoServer.java
+++ TCPworkProject/src/Server/UDPEchoServer.java
@@ -1,8 +1,13 @@
 package Server;
 
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 
 public class UDPEchoServer {
+	
+	private static final int ECHOMAX = 255;
+	
 	public static void main(String[] args)throws IOException{
 		
 		new UDPEchoServer().exec(args);
@@ -10,9 +15,19 @@ public class UDPEchoServer {
 	}
 	public void exec(String args[])throws IOException{
 		
+		if(args.length != 1){
+			throw new IllegalArgumentException("Parameter(s) : <Port>");
+		}
 		
+		int servPort = Integer.parseInt(args[0]);
+		DatagramSocket socket = new DatagramSocket(servPort);
+		DatagramPacket packet = new DatagramPacket(new byte[ECHOMAX],ECHOMAX);
 		
-		
+		for(;;){
+			socket.receive(packet);
+			System.out.println("Handling client at " + packet.getAddress().getHostAddress() + " on port " + packet.getPort());
+			socket.send(packet);
+			packet.setLength(ECHOMAX);
+		}
 	}
-
 }
