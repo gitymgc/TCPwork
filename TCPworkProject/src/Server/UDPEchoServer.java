ad3a78ee7cbb0abbb99940e33e5689bfc692412f
Yamaguchi <Yamaguchi@192.168.101.104>
2013-08-07 10:29:22 +0900

    UDEPEchoServer作成

---
+++ TCPworkProject/src/Server/UDPEchoServer.java
@@ -0,0 +1,18 @@
+package Server;
+
+import java.io.IOException;
+
+public class UDPEchoServer {
+	public static void main(String[] args)throws IOException{
+		
+		new UDPEchoServer().exec(args);
+		
+	}
+	public void exec(String args[])throws IOException{
+		
+		
+		
+		
+	}
+
+}
