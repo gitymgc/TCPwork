d2661a9a370e0e06ae8eb88cea16135491da91b0
Yamaguchi <Yamaguchi@192.168.101.104>
2013-08-15 10:28:41 +0900

    ThreadExmapleクラス作成、完成

---
+++ TCPworkProject/src/Server/ThreadExample.java
@@ -0,0 +1,25 @@
+package Server;
+
+public class ThreadExample implements Runnable{
+
+	private String greeting;
+
+	public ThreadExample(String greeting){
+		this.greeting = greeting;
+
+	}
+	public void run(){
+		for(;;){
+			System.out.println(Thread.currentThread().getName() + ": "+ greeting);
+			try{
+				Thread.sleep((long)(Math.random() * 100));
+			}catch(InterruptedException e){}
+		}
+	}
+	
+	public static void main(String args[]){
+		new Thread(new ThreadExample("Hello")).start();
+		new Thread(new ThreadExample("Aloha")).start();
+		new Thread(new ThreadExample("Ciao")).start();
+	}
+}
