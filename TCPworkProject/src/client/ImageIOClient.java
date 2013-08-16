d4a60d1ba760735d0946911ba9c08ee7e1bb1eb9
Yamaguchi <Yamaguchi@192.168.101.104>
2013-08-16 17:08:06 +0900

    ImageIO仕様

--- TCPworkProject/src/client/ImageIOClient.java
+++ TCPworkProject/src/client/ImageIOClient.java
@@ -2,8 +2,6 @@ package client;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
@@ -23,6 +21,7 @@ public class ImageIOClient {
 
 		String srcDirPath = "./resources/debug/src/";
 		String dstDirPath = "./resources/debug/dst/";
+		
 		File srcDir = new File(srcDirPath);
 		File srcFiles[] = srcDir.listFiles();
 
@@ -36,50 +35,19 @@ public class ImageIOClient {
 
 			InputStream is = sock.getInputStream();
 			OutputStream os = sock.getOutputStream();
-			//			BufferedOutputStream out = 
-			//					new BufferedOutputStream(
-			//					sock.getOutputStream()
-			//					);
-
-			//送信
-			FileInputStream fis = new FileInputStream(srcFile);
-			int count = 0;
-			int data = 0;
-			// バッファのサイズ
-			int bufSize = 4096;
-			int recvMsgSize;
-			byte buf[] = new byte[bufSize];
-			while( (recvMsgSize = fis.read(buf) ) != -1){
-				os.write(buf);
-			}
-			os.write(1);
-
-			//送信確認
-			int res = 0;
-			res = is.read();
-			if(res == 1){
-				System.out.println("このファイル形式にはサーバは対応していません。");
-				sock.close();
-				continue;
-			} else {
-
-			}
+			
+			BufferedImage srcImg = ImageIO.read(srcFile);
+			String name = srcFile.getName();
+			String sepName[] = name.split("\\.");
+			ImageIO.write(srcImg, sepName[1],os);
+			
 			System.out.println("処理開始");
 			
 			//受信
+			BufferedImage dstImg = ImageIO.read(is);
 			String dstFilePath = dstDirPath + srcFile.getName()+ ".bmp";
 			File dstFile = new File(dstFilePath);
-			FileOutputStream fos = new FileOutputStream(dstFile);
-			byte elemBuf[] =  new byte[4096];
-			while((recvMsgSize = is.read(elemBuf)) != 1){
-				fos.write(elemBuf);
-			}
-			BufferedImage dstImg = ImageIO.read(dstFile);
-//			if(dstImg == null)continue;
 			ImageIO.write(dstImg, "bmp", dstFile);
-			fos.flush();
-			fos.close();
-			fis.close();
 			sock.close();
 		}
 	}
