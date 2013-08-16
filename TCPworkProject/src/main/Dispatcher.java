9cbd41d0331ef734e872861a3be4ab8201b255f4
Yamaguchi <Yamaguchi@192.168.101.104>
2013-08-16 09:29:38 +0900

    Dispatcherクラス完成。

---
+++ TCPworkProject/src/main/Dispatcher.java
@@ -0,0 +1,8 @@
+package main;
+
+import java.net.ServerSocket;
+
+public interface Dispatcher {
+	public void startDispatching(ServerSocket servSock, Logger logger, ProtocolFactory protoFactory);
+
+}
