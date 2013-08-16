246df95266d53ce07cf860a0f35722ef036936c7
Yamaguchi <Yamaguchi@192.168.101.104>
2013-08-16 09:29:15 +0900

    EchoProtocolFactory完成。

---
+++ TCPworkProject/src/main/EchoProtocolFactory.java
@@ -0,0 +1,9 @@
+package main;
+
+import java.net.Socket;
+
+public  class EchoProtocolFactory implements ProtocolFactory{
+	public Runnable createProtocol(Socket clntSock, Logger logger){
+		return new EchoProtocol(clntSock, logger);
+	}
+}
